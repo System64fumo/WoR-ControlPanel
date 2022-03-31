5841a3857802ad11d9257410a410bee68ecf22ff
Amir Dahan <72354122+AmirDahan@users.noreply.github.com>
2022-03-31 03:19:45 +0300

    Fixed build errors

--- Main/Properties/AssemblyInfo.cs
+++ Main/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("756eeacc-8a08-43d2-953c-6868310cfd00")]
 
-[assembly: AssemblyVersion("0.3.4.0")]
-[assembly: AssemblyFileVersion("0.3.4.0")]
+[assembly: AssemblyVersion("0.3.3.5")]
+[assembly: AssemblyFileVersion("0.3.3.5")]
